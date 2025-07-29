5e7ae9569fbe573a1279a7d0cb85f3d7a0d1f61c
erbroome <ellenehall@gmail.com>
2025-07-28 21:39:06 -0500

    webpack-intro assignment

---
+++ src/greetings.js
@@ -0,0 +1,9 @@
+console.log("Hello, from greetings.js!");
+
+export function sayHello(name) {
+  return `Hello, ${name}!`;
+}
+
+export function sayGoodbye(name) {
+  return `Goodbye, ${name}!`;
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+console.log("Hello, from index.js!");
+
+import { sayHello, sayGoodbye } from './greetings.js';
+console.log(sayHello("Webpack"));
+console.log(sayGoodbye("Webpack"));
